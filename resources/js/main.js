64b4749066f5932293650b2681af88c2daf7a870
Daryl del Rosario <daryldelrosario.is@gmail.com>
2023-05-08 13:04:28 -0400

    complete list sorter full function with input and table

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,29 @@
-// ADD ROW FUNCTION 
+// =====================================
+// SETTING UP THE TABLE WITH PRESET DATA
+// =====================================
+// FUNCTION: PRESET ROW
+function presetRow(fname, lname, age) {
+    const row = $("<tr>");
+    const fnameCell = $("<td>").text(fname);
+    const lnameCell = $("<td>").text(lname);
+    const ageCell = $("<td>").text(age);
+    row.append(fnameCell, lnameCell, ageCell);
+
+    $("#data-table tbody").append(row);
+}
+
+// Preset the table with some data
+presetRow("Daryl", "del Rosario", 36);
+presetRow("Clark Kent", "del Rosario", 1);
+presetRow("Ashley", "Martins Pimentel", 35);
+presetRow("Aqua", "Man", 10);
+presetRow("Cat", "Woman", 2);
+presetRow("Hello", "World", 20);
+
+// ==================================
+// CREATING ONCLICK FUNCTION addRow()
+// ==================================
+// FUNCTION: ADD ROW
 function addRow() {
     const firstName = $("#firstName").val();
     const lastName = $("#lastName").val();
@@ -17,16 +42,39 @@ function addRow() {
     $("#age").val("");
 }
 
-function presetRow(fname, lname, age) {
-    const row = $("<tr>");
-    const fnameCell = $("<td>").text(fname);
-    const lnameCell = $("<td>").text(lname);
-    const ageCell = $("<td>").text(age);
-    row.append(fnameCell, lnameCell, ageCell);
+// ============================================
+// CREATING SORTING FUNCTION sortTable(a, b, c)
+// ============================================
+// FUNCTION: SORT TABLE 
+function sortTable(table, columnIndex, isNumeric) {
+    const rows = table.find("tbody > tr").get();
+    rows.sort(function(a, b) {
+        const cellA = $(a).find("td").eq(columnIndex).text();
+        const cellB = $(b).find("td").eq(columnIndex).text();
+        if(isNumeric) {
+            return Number(cellA) - Number(cellB);
+        } else {
+            return cellA.localeCompare(cellB);
+        }
+    });
 
-    $("#data-table tbody").append(row);
+    const currentOrder = $(table.find("th").get(columnIndex)).attr("data-order");
+    if(currentOrder === "asc") {
+        rows.reverse();
+        $(table.find("th").get(columnIndex)).attr("data-order", "desc");
+    } else {
+        $(table.find("th").get(columnIndex)).attr("data-order", "asc");
+    }
+
+    $.each(rows, function(index, row) {
+        table.children("tbody").append(row);
+    });
 }
 
-presetRow("Daryl", "del Rosario", 36);
-presetRow("Clark Kent", "del Rosario", 1);
-presetRow("Ashley", "Martins Pimentel", 35);
+// Adding click event handlers to the table headers for sorting 
+$("#data-table th").click(function() {
+    const table = $(this).closest("table");
+    const columnIndex = $(this).index();
+    const isNumeric = columnIndex === 2;
+    sortTable(table, columnIndex, isNumeric);
+});
